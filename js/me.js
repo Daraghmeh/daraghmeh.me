5483695e7c8b6f494637a03e29a8baa1963d5962
Mohammad Daraghmeh <daraghmeh.mohammad1992@gmail.com>
2016-02-23 00:04:48 -0500

    Added FormSpree.io
    Added FormSpree.io in contact section.

--- js/me.js
+++ js/me.js
@@ -31,3 +31,48 @@ $('body > *').not('nav').click(function() {
         $('.navbar-toggle:visible').click();
     }
 });
+
+// Validates the contact form and submits it using Formspree.
+$.validator.setDefaults({
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
+
+$("#contact-form").validate({
+    submitHandler: function(form) {
+        $.ajax({
+            url: "//formspree.io/mohammad@daraghmehdigital.com",
+            method: "POST",
+            data: {
+                name: $(form).find("input[name='name']").val(),
+                email: $(form).find("input[name='email']").val(),
+                subject: $(form).find("input[name='_subject']").val(),
+
+
+                phone: $(form).find("input[name='phone']").val(),
+                message: $(form).find("textarea[name='message']").val()
+            },
+            dataType: "json",
+            success: function() {
+                $("#submit-success").fadeIn();
+                $("#contact-form").fadeOut();
+            },
+            error: function() {
+                $("#submit-errors").fadeIn();
+            }
+        });
+    }
+});
